44ee1fd99b17b395b258137b5c9840d5c428151c
jirawan22 <56579285+jirawan22@users.noreply.github.com>
2019-10-16 17:16:46 +0700

    3_6Testing_Methods_that_Throw_Exceptions

--- TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
+++ TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -26,19 +26,7 @@ namespace TestNinja.UnitTests
             var logger = new ErrorLogger();
 
             Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
-        }
 
-        [Test]
-        public void Log_ValidError_RaiseErrorLoggedEvent()
-        {
-            var logger = new ErrorLogger();
-
-            var id = Guid.Empty;
-            logger.ErrorLogged += (sender, args) => { id = args; };
-
-            logger.Log("a");
-
-            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
 
         }
 
--- TestNinja/TestNinja/Fundamentals/ErrorLogger.cs
+++ TestNinja/TestNinja/Fundamentals/ErrorLogger.cs
@@ -11,6 +11,7 @@ namespace TestNinja.Fundamentals
 
         public void Log(string error)
         {
+    
             if (String.IsNullOrWhiteSpace(error))
                 throw new ArgumentNullException();
 
