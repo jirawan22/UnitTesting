0d10fcc41b6a65af449f46cde43b463c7ebaac0f
jirawan22 <56579285+jirawan22@users.noreply.github.com>
2019-10-16 17:21:23 +0700

    3_7Testing_Methods_that_Raise_an_Event

--- TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
+++ TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -26,7 +26,19 @@ namespace TestNinja.UnitTests
             var logger = new ErrorLogger();
 
             Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
+        }
 
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var logger = new ErrorLogger();
+
+            var id = Guid.Empty;
+            logger.ErrorLogged += (sender, args) => { id = args; };
+
+            logger.Log("a");
+
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
 
         }
 
