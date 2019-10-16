aeb1b60ae765dcbf58e491274c9e1e3202680d51
jirawan22 <56579285+jirawan22@users.noreply.github.com>
2019-10-16 20:42:24 +0700

    4_4_Solution_DemeritPointsCalculator

--- TestNinja/TestNinja/Fundamentals/DemeritPointsCalculator.cs
+++ TestNinja/TestNinja/Fundamentals/DemeritPointsCalculator.cs
@@ -5,10 +5,11 @@ namespace TestNinja.Fundamentals
     public class DemeritPointsCalculator
     {
         private const int SpeedLimit = 65;
+        private const int MaxSpeed = 300;
 
         public int CalculateDemeritPoints(int speed)
         {
-            if (speed < 0)
+            if (speed < 0 || speed > MaxSpeed)
                 throw new ArgumentOutOfRangeException();
 
             if (speed <= SpeedLimit) return 0;
