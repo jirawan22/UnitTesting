5ff15e1aadacfaeb856cf9a5a4b24bafb837d574
jirawan22 <56579285+jirawan22@users.noreply.github.com>
2019-10-16 13:50:57 +0700

    Create

---
+++ TestNinja/TestNinja.UnitTests/ResevationTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.UnitTests
+{
+    class ResevationTests
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
