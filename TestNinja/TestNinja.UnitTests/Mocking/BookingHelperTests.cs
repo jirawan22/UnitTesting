4043e3237b8c69d425c2496c46d556b80c535bbe
jirawan22 <56579285+jirawan22@users.noreply.github.com>
2019-10-17 22:01:49 +0700

    7_2_Test_Cases

---
+++ TestNinja/TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class BookingHelper_OverlappingBookingsExistTests
+    {
+        [Test]
+        public void BookingStarsAndFinisheBeforeAnExistingBooking_ReturnEmptyString()
+        {
+
+        }
+    }
+}
